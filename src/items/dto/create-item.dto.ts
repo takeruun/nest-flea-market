8246d0df06ebc082131f8f9346be685ab64c26fa
takeru.hashimoto <hstake25@gmail.com>
2022-09-29 01:58:49 +0900

    クラスバリデーション

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,5 +1,17 @@
+import { Type } from 'class-transformer';
+import { MaxLength, IsString, IsNotEmpty, IsInt, Min } from 'class-validator';
 export class CreateItemDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(40)
   name: string;
+
+  @IsInt()
+  @Min(1)
+  @Type(() => Number) // string で渡ってくるものを number にする
   price: number;
+
+  @IsString()
+  @IsNotEmpty()
   describe: string;
 }
